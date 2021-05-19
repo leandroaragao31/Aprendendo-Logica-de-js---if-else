dd31efaf63651e8413d3279f8698237605ff9983
leandroaragao31 <leandroaragao31@gmail.com>
2021-05-19 12:03:50 -0300

    corrigindo desafio bonus

--- script.js
+++ script.js
@@ -25,20 +25,20 @@ if((month === "Janeiro" ) || (month === "Dezembro")){
 //Exercicio bonus, Resposta 4
 
 let Name = "Reinaldo"
-browserType = [0]
 
-if(browserType.indexOf("R") ==  -1){
-    console.log("Nome começa com a Letra R")
-}else {
-    console.log("Não começa com a Letra R")
+
+if(Name.substring(0,1) == "R"){
+    console.log("o Nome começa com a letra R")
+}else{
+    console.log("o Nome não começa com a letra R")
 }
 
 //Exercicio Bonus, Resposta 5
 
-let lastName = "Oliveira" 
-browsertype =[0]
+let lastName = "Oliver" 
 
-if((lastName.length > 6  ) || (browsertype.indexOF("E") == -1)){
+
+if((lastName.length > 6  ) || (lastName.substring(0,1) == "E")){
     console.log("tem mais de 6 letras ou seu nome começa com a letra E")
 }else {
     console.log("não tem mais de 6 letras ou seu nome não começa com E")
